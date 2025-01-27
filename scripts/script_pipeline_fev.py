cc9a2502bbc618e55d864ce46142acf2ece69175
tmatsbr <tiagomars233@gmail.com>
2025-01-27 19:01:38 -0300

    "unindo dados"

--- scripts/script_pipeline_fev.py
+++ scripts/script_pipeline_fev.py
@@ -93,6 +93,34 @@ def rename_columns(dados, key_mapping):
         new_dados_csv.append(dict_temp)
     return new_dados_csv
 
+def size_data(dados):
+    """
+    Retorna o tamanho dos dados.
+    
+    Args:
+    dados (list): Lista de dicionários contendo os dados.
+    
+    Returns:
+    int: Tamanho dos dados.
+    """
+    return len(dados)
+
+def join(dadosA, dadosB):
+    """
+    Junta dois conjuntos de dados em uma única lista.
+    
+    Args:
+    dadosA (list): Primeiro conjunto de dados.
+    dadosB (list): Segundo conjunto de dados.
+    
+    Returns:
+    list: Lista combinada dos dois conjuntos de dados.
+    """
+    combined_list = []
+    combined_list.extend(dadosA)
+    combined_list.extend(dadosB)
+    return combined_list
+
 # %% [markdown]
 # # Caminhos dos arquivos de dados
 
@@ -106,11 +134,17 @@ path_csv = '../dados_brutos/dados_empresaB.csv'
 # %%
 dados_json = leitura_dados(path_json, 'json')
 nome_colunas_json = get_columns(dados_json)
+tamanho_dados_json = size_data(dados_json)
+
 print(f"Nome colunas dados JSON: {nome_colunas_json}")
+print(f"Tamanho dos dados JSON: {tamanho_dados_json}")
 
 dados_csv = leitura_dados(path_csv, 'csv')
 nome_colunas_csv = get_columns(dados_csv)
+tamanho_dados_csv = size_data(dados_csv)
+
 print(f"Nome colunas dados CSV: {nome_colunas_csv}")
+print(f"Tamanho dos dados CSV: {tamanho_dados_csv}")
 
 # %% [markdown]
 # # Transformação dos dados
@@ -130,5 +164,16 @@ key_mapping = {
 dados_csv = rename_columns(dados_csv, key_mapping)
 nome_colunas_csv = get_columns(dados_csv)
 print(f"Nome colunas dados CSV após renomeação: {nome_colunas_csv}")
+
+# %% [markdown]
+# # Unindo os dados
+
+# %%
+dados_fusao = join(dados_json, dados_csv)
+nomes_colunas_fusao = get_columns(dados_fusao)
+tamanho_dados_fusao = size_data(dados_fusao)
+
+print(f"Nome colunas dados fusão: {nomes_colunas_fusao}")
+print(f"Tamanho dos dados fusão: {tamanho_dados_fusao}")
 
 # %%
