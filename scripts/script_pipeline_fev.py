81f95bb43345665575af1ed925bbd87d9dafc329
tmatsbr <tiagomars233@gmail.com>
2025-01-27 18:52:48 -0300

    "transformando os dados"

--- scripts/script_pipeline_fev.py
+++ scripts/script_pipeline_fev.py
@@ -1,24 +1,42 @@
 # %% [markdown]
-# # Exploração de Dados - Fusão Clínicas Sanare e Reviver
+# # Pipeline de Dados - Fusão Clínicas Sanare e Reviver
 # 
-# Este notebook contém a análise exploratória dos dados das duas clínicas.
+# Este script contém o pipeline de dados para a fusão das duas clínicas.
 
 # %%
-
+# Importando bibliotecas necessárias
 import json
 import csv
 
 # %% [markdown]
-# # lendo dados 
+# # Funções de leitura de dados
 
 # %%
 def leitura_json(path_json):
+    """
+    Lê um arquivo JSON e retorna os dados como uma lista de dicionários.
+    
+    Args:
+    path_json (str): Caminho para o arquivo JSON.
+    
+    Returns:
+    list: Dados do arquivo JSON.
+    """
     dados_json = []
     with open(path_json, 'r') as file:
         dados_json = json.load(file)
     return dados_json
 
 def leitura_csv(path_csv):
+    """
+    Lê um arquivo CSV e retorna os dados como uma lista de dicionários.
+    
+    Args:
+    path_csv (str): Caminho para o arquivo CSV.
+    
+    Returns:
+    list: Dados do arquivo CSV.
+    """
     dados_csv = []
     with open(path_csv, 'r') as file:
         spamreader = csv.DictReader(file, delimiter=',')
@@ -27,6 +45,16 @@ def leitura_csv(path_csv):
     return dados_csv
 
 def leitura_dados(path, tipo_arquivo):
+    """
+    Lê um arquivo de dados (JSON ou CSV) e retorna os dados como uma lista de dicionários.
+    
+    Args:
+    path (str): Caminho para o arquivo de dados.
+    tipo_arquivo (str): Tipo do arquivo ('json' ou 'csv').
+    
+    Returns:
+    list: Dados do arquivo.
+    """
     dados = []
     if tipo_arquivo == 'csv':
         dados = leitura_csv(path)
@@ -34,13 +62,73 @@ def leitura_dados(path, tipo_arquivo):
         dados = leitura_json(path)
     return dados
 
+def get_columns(dados):
+    """
+    Retorna os nomes das colunas dos dados.
+    
+    Args:
+    dados (list): Lista de dicionários contendo os dados.
+    
+    Returns:
+    list: Lista com os nomes das colunas.
+    """
+    return list(dados[0].keys())
+
+def rename_columns(dados, key_mapping):
+    """
+    Renomeia as colunas dos dados de acordo com o mapeamento fornecido.
+    
+    Args:
+    dados (list): Lista de dicionários contendo os dados.
+    key_mapping (dict): Dicionário de mapeamento das colunas.
+    
+    Returns:
+    list: Lista de dicionários com as colunas renomeadas.
+    """
+    new_dados_csv = []
+    for old_dict in dados:
+        dict_temp = {}
+        for old_key, value in old_dict.items():
+            dict_temp[key_mapping.get(old_key, old_key)] = value
+        new_dados_csv.append(dict_temp)
+    return new_dados_csv
+
+# %% [markdown]
+# # Caminhos dos arquivos de dados
+
+# %%
 path_json = '../dados_brutos/dados_empresaA.json'
 path_csv = '../dados_brutos/dados_empresaB.csv'
 
+# %% [markdown]
+# # Leitura dos dados
+
+# %%
 dados_json = leitura_dados(path_json, 'json')
-print(dados_json[0])
+nome_colunas_json = get_columns(dados_json)
+print(f"Nome colunas dados JSON: {nome_colunas_json}")
 
 dados_csv = leitura_dados(path_csv, 'csv')
-print(dados_csv[0])
+nome_colunas_csv = get_columns(dados_csv)
+print(f"Nome colunas dados CSV: {nome_colunas_csv}")
+
+# %% [markdown]
+# # Transformação dos dados
+
+# %%
+# Mapeamento das chaves do CSV para os nomes desejados
+key_mapping = {
+    'Nome do Item': 'Nome do Produto',
+    'Classificação do Produto': 'Categoria do Produto',
+    'Valor em Reais (R$)': 'Preço do Produto (R$)',
+    'Quantidade em Estoque': 'Quantidade de Estoque',
+    'Nome da Loja': 'Filial',
+    'Data da Venda': 'Data da Venda'
+}
+
+# Renomeando as colunas do CSV
+dados_csv = rename_columns(dados_csv, key_mapping)
+nome_colunas_csv = get_columns(dados_csv)
+print(f"Nome colunas dados CSV após renomeação: {nome_colunas_csv}")
 
 # %%
